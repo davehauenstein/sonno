e87f78dd9abe69577ccd1b4c10386d91e1e07999
Tharsan Bhuvanendran <me@tharsan.com>
2012-10-09 22:50:56 -0400

    Added methods implemented in the Request concrete implementation, to the interface.

--- src/Sonno/Http/Request/RequestInterface.php
+++ src/Sonno/Http/Request/RequestInterface.php
@@ -37,6 +37,114 @@ interface RequestInterface
     public function getRequestUri();
 
     /**
+     * Return the content length of the body sent in the request.
+     *
+     * @return int
+     */
+    public function getContentLength();
+
+    /**
+     * Return the content type of the body sent in the request.
+     *
+     * @returns string
+     */
+    public function getContentType();
+
+    /**
+     * Return an array of headers.
+     *
+     * @return array
+     */
+    public function getHeaders();
+
+    /**
+     * Return the raw, unmodified request body.
+     *
+     * A client may specify content in the request body. This method will
+     * read in that content and return it completely unaltered or formatted.
+     *
+     * @return string
+     */
+    public function getRequestBody();
+
+    /**
+     * Return an array of query parameters.
+     *
+     * @return array
+     */
+    public function getQueryParams();
+
+    /**
+     * Return a specific query parameter.
+     *
+     * @return string
+     */
+    public function getQueryParam($param);
+
+    /**
+     * Return whether or not this request uses a secure connection (https).
+     *
+     * @return boolean
+     */
+    public function isSecure();
+
+    /**
+     * Return the port number used for the request.
+     *
+     * @return int
+     */
+    public function getPort();
+
+    /**
+     * Convenience method for determining if the request is a GET request.
+     *
+     * @return boolean
+     */
+    public function isGet();
+
+    /**
+     * Convenience method for determining if the request is a POST request.
+     *
+     * @return boolean
+     */
+    public function isPost();
+
+    /**
+     * Convenience method for determining if the request is a PUT request.
+     *
+     * @return boolean
+     */
+    public function isPut();
+
+    /**
+     * Convenience method for determining if the request is a DELETE request.
+     *
+     * @return boolean
+     */
+    public function isDelete();
+
+    /**
+     * Convenience method for determining if the request is a HEAD request.
+     *
+     * @return boolean
+     */
+    public function isHead();
+
+    /**
+     * Convenience method for determining if the request is a TRACE request.
+     *
+     * @return boolean
+     */
+    public function isTrace();
+
+    /**
+     * Convenience method for determining if the request is a OPTIONS request.
+     *
+     * @return boolean
+     */
+    public function isOptions();
+
+    /**
      * Select the representation variant that best matches the request.
      * More explicit variants are chosen ahead of less explicit ones.
      *
